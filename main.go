9eb0c82d5b2227941f638459eb60c6bb1a7ae24d
nesyasal <nesya.sr@gmail.com>
2025-11-11 00:10:45 +0700

    add cors and logging

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 
 	"backend-elearning/database"
 
-	"github.com/gofiber/fiber/v2"
 	"backend-elearning/routes"
+
+	loggermw "github.com/gofiber/fiber/v2/middleware/logger"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -17,6 +21,19 @@ func main() {
 	database.ConnectDB(cfg)
 
 	app := fiber.New()
+
+	// Middleware: CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://127.0.0.1:5501",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+	}))
+
+	app.Use(loggermw.New(loggermw.Config{
+		// Format: [Waktu] IP [WarnaStatus]Status[ResetWarna] - Latensi Method Path
+		Format: "[${time}] ${ip} ${color}${status}${reset} - ${latency} ${method} ${path}\n",
+	}))
 
 	routes.SetupRoutes(app)
 
